a72dd1640e4bf6b95e27930aeb467ab826d66da0
Stefan Dehelean <stefan.dehelean@aqvangarde-software.com>
2022-09-27 10:51:54 +0300

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import './i18n/i18n';
 import { useTranslation } from 'react-i18next';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
-import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
@@ -11,6 +17,44 @@ function App() {
     <div className="App">
       <header className="App-header">
         {t('Welcome to React')}
+
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            {/*
+              A <Switch> looks through all its children <Route>
+              elements and renders the first one whose path
+              matches the current URL. Use a <Switch> any time
+              you have multiple routes, but you want only one
+              of them to render at a time
+            */}
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/dashboard">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
